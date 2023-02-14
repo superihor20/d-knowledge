364e7518ed63412a73f4f32c74b568851afafbba
Ihor Naidonov <superihor20@gmail.com>
2023-02-14 10:05:35 +0200

    added some rules to eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,31 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    'next',
+    'turbo',
+    'prettier',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
