347196c9442091ccdfdf14871470af8428004cb4
Ihor Naidonov <superihor20@gmail.com>
2023-02-14 12:44:27 +0200

    expoting apis

---
+++ packages/api/src/modules/index.ts
@@ -0,0 +1,8 @@
+export * from './auth/auth.api';
+export type { AuthResponse, UserCredentials } from './auth/auth.schemas';
+
+export * from './question-category/question-category.api';
+export type {
+  QuestionCategory,
+  QuestionCategoryInput,
+} from './question-category/question-category.schema';
