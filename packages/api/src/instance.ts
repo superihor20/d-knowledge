e32c6b178bc74d9edd61a010460a42090f41e446
Ihor Naidonov <superihor20@gmail.com>
2023-02-14 12:31:12 +0200

    instance for api

---
+++ packages/api/src/instance.ts
@@ -0,0 +1,28 @@
+import Axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
+
+export const axios = Axios.create({
+  // TODO: configurate global env
+  baseURL: 'baseUrl', //process?.env?.NEXT_PUBLIC_API,
+});
+
+const authRequestInterceptor = (
+  config: InternalAxiosRequestConfig
+): InternalAxiosRequestConfig => {
+  const updatedConfig = { ...config };
+  const token = localStorage.getItem('accessToken');
+
+  if (token) {
+    updatedConfig.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+  }
+
+  return updatedConfig;
+};
+
+axios.interceptors.request.use(authRequestInterceptor);
+
+axios.interceptors.response.use(
+  (response) => response.data,
+  (error: AxiosError) => {
+    throw error.response?.data;
+  }
+);
